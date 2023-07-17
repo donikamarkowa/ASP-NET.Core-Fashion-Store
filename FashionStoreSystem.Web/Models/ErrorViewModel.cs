e5098cbd18b499207c275c450f808b9c6fb7708b
donikamarkowa <donimarkova@gmail.com>
2023-07-17 10:07:24 +0300

    Add project files.

---
+++ FashionStoreSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FashionStoreSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
